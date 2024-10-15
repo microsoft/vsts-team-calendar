f456ed5ca489dc2c57a62a66b54f8b30ca1cc33f
Madis Kõosaar <mkoosaar@live.co.uk>
2024-10-15 11:43:20 +0300

    revert changes to work with 2019

--- legacy/webpack.config.js
+++ legacy/webpack.config.js
@@ -1,32 +1,37 @@
 const path = require("path");
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
     filename: path.relative(process.cwd(), path.join(__dirname, "dist", "css", "style.css"))
 });
 
 module.exports = env => {
-    const plugins = [extractSass];
-    if (env && env.substring(0, 4) === "prod") {
-        plugins.push(
-            new webpack.optimize.UglifyJsPlugin({
-                compress: {
-                    warnings: false
-                },
-                output: {
-                    comments: false
-                }
-            })
-        );
-    }
+    
+    const plugins = [];
+    // if (env && env.substring(0, 4) === "prod") {
+    //     plugins.push(
+    //         new webpack.optimize.UglifyJsPlugin({
+    //             compress: {
+    //                 warnings: false
+    //             },
+    //             output: {
+    //                 comments: false
+    //             }
+    //         })
+    //     );
+    // }
     return {
+        mode: "development",
         entry: {
             main: "./" + path.relative(process.cwd(), path.join(__dirname, "src", "Calendar", "Extension.tsx")),
             calendarServices: "./" + path.relative(process.cwd(), path.join(__dirname, "src", "Calendar", "CalendarServices.ts")),
             dialogs: "./" + path.relative(process.cwd(), path.join(__dirname, "src", "Calendar", "Dialogs.ts"))
         },
         output: {
-            filename: path.relative(process.cwd(), path.join(__dirname, "dist", "js", "[name].js")),
+            filename: path.relative(process.cwd(), path.resolve(__dirname, "js", "[name].js")),
+            // filename: '[name].js',
+            // path: path.resolve(__dirname, 'dist', 'js'),
             libraryTarget: "amd"
         },
         externals: [
@@ -46,23 +51,31 @@ module.exports = env => {
         module: {
             rules: [
                 { test: /\.tsx?$/, loader: "ts-loader" },
+                // {
+                //     test: /\.scss$/,
+                //     use: extractSass.extract({
+                //         use: [
+                //             {
+                //                 loader: "css-loader",
+                //                 options: { importLoaders: 1 }
+                //             },
+                //             {
+                //                 loader: "sass-loader"
+                //             },
+                //             {
+                //                 loader: "postcss-loader"
+                //             }
+                //         ],
+                //         fallback: "style-loader"
+                //     })
+                // },
                 {
                     test: /\.scss$/,
-                    use: extractSass.extract({
-                        use: [
-                            {
-                                loader: "css-loader",
-                                options: { importLoaders: 1 }
-                            },
-                            {
-                                loader: "sass-loader"
-                            },
-                            {
-                                loader: "postcss-loader"
-                            }
-                        ],
-                        fallback: "style-loader"
-                    })
+                    use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                    ]
                 },
                 {
                     test: /\.css$/,
@@ -70,6 +83,17 @@ module.exports = env => {
                 }
             ]
         },
-        plugins: plugins
+        plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "**/*.html", to: "./dist", context: "static" },
+        { from: "node_modules/vss-web-extension-sdk/lib", to: "./sdk"},
+        { from: "node_modules/moment/min", to: "./lib/moment"},
+        { from: "node_modules/jquery/dist", to: "./lib/jquery"},
+        { from: "node_modules/fullcalendar/dist", to: "./lib/fullcalendar"}
+        
+      ]
+    })
+  ]
     };
 };
